fbe2c5f5e728ac169d78f07fc1f005a7e9db5f53
MaKarastY <71918286+makarasty@users.noreply.github.com>
2023-06-28 14:06:42 +0300

    🟢

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { request } = require('https')
+const { lookup } = require('dns')
 /**
  * @param {URL|string} link Server url
  * @param {Promise|undefined} promise Promice from you promices library
@@ -6,20 +7,21 @@ const { request } = require('https')
  * @returns JSON object with data about the server url
  */
 const pingerMinecraft = (link, promise = Promise, json = JSON) => new promise((resolve, reject) => {
-	if (!link) resolve(new Error('The server IP should be passed using the function argument!'))
 	this.chunks = ''
-	/**
-	 * @private
-	 */
-	const addChunk = (chunk) => this.chunks += chunk
-	this.options = json.stringify({ "server": link })
-	request({
-		hostname: 'www.minecraftpinger.com',
-		path: '/api/get-server-status',
-		method: 'POST',
-		headers: { "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0", 'Content-Type': 'text/plain;charset=UTF-8', 'Content-Length': this.options.length }
-	}, (resource) => void resource.on('data', addChunk).on('end', () => {
-		try { resolve(json.parse(this.chunks)) } catch (error) { reject(new Error('Failed to get a response from the api\n' + link + '\n' + this.chunks)) }
-	})).on('error', reject).end(this.options)
+	const [ip, port] = link.split(':')
+	lookup(ip, (error, address = ip) => {
+		if (error) reject(new Error('Failed to get a response from the server ip\n' + error))
+		this.options = json.stringify({ "server": link })
+		request({
+			hostname: 'www.minecraftpinger.com',
+			path: '/api/get-server-status',
+			method: 'POST',
+			headers: { "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0", 'Content-Type': 'text/plain;charset=UTF-8', 'Content-Length': this.options.length }
+		}, (resource) => void resource.on('data', (chunk) => this.chunks += chunk).on('end', () => {
+			try {
+				const data = json.parse(this.chunks); data.server.addresss = address; resolve(data)
+			} catch (error) { reject(new Error('Failed to get a response from the api\n' + link + '\n' + this.chunks)) }
+		})).on('error', reject).end(this.options)
+	})
 })
 module.exports = { pingerMinecraft }
