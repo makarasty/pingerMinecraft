09bf1a5b4f4c558a6ca26e28de7763e703074818
makarasty <makarasty123@gmail.com>
2024-07-16 14:20:02 +0300

    update

--- example.js
+++ example.js
@@ -1,21 +1,21 @@
 const pingerMinecraft = require("./index.js") // require("pingerminecraft")
 
 async function logMinecraftServerStats() {
-	const minecraftServerStats = await pingerMinecraft('95.216.62.176:25629')
+	const minecraftServerStats = await pingerMinecraft('95.216.62.176:25565')
 
 	console.info(minecraftServerStats)
 }
 
 logMinecraftServerStats()
 
-pingerMinecraft('95.216.62.176:25629')
+pingerMinecraft('95.216.62.176:25565')
 	.then(data => console.log(data))
 	.catch(error => console.error(error))
 
-pingerMinecraft('95.216.62.176:25629', true)
+pingerMinecraft('95.216.62.176:25566', true)
 	.then(data => console.log(data))
 	.catch(error => console.error(error))
 
-pingerMinecraft('95.216.62.176:25629')
+pingerMinecraft('go.nextgrief.pw')
 	.then(console.log)
 	.catch(console.error)
